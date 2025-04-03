e66762f5a1c2fd7912cc140add56aa51f039f597
Jolvera2001 <jolvera6@stedwards.edu>
2025-04-02 22:58:50 -0500

    starting on actual app

---
+++ src/main/java/com/jolvera/jbud/domain/Expense.java
@@ -0,0 +1,6 @@
+package com.jolvera.jbud.domain;
+
+import com.jolvera.jbud.domain.abstracts.RecurringExpense;
+
+public class Expense extends RecurringExpense {
+}
---
+++ src/main/java/com/jolvera/jbud/domain/abstracts/RecurringExpense.java
@@ -0,0 +1,4 @@
+package com.jolvera.jbud.domain.abstracts;
+
+public abstract class RecurringExpense {
+}
