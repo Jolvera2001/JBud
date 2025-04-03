72e5ad22df8a9c05299972aea17de209d25f11c2
Jolvera2001 <jolvera6@stedwards.edu>
2025-04-02 23:26:23 -0500

    implemented RecurrencePattern

--- src/main/java/com/jolvera/jbud/domain/enums/RecurrencePattern.java
+++ src/main/java/com/jolvera/jbud/domain/enums/RecurrencePattern.java
@@ -1,4 +1,21 @@
 package com.jolvera.jbud.domain.enums;
 
 public enum RecurrencePattern {
+    DAILY("Daily"),
+    WEEKLY("Weekly"),
+    BIWEEKLY("Biweekly"),
+    MONTHLY("Monthly"),
+    QUARTERLY("Quarterly"),
+    SEMI_ANNUAL("Semi-annual"),
+    ANNUAL("Annual");
+
+    private final String displayName;
+
+    RecurrencePattern(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
