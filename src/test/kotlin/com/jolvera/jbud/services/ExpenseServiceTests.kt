14bbd75b1b8b2f66ead37758e56b8fd71e842360
Jolvera2001 <jolvera6@stedwards.edu>
2025-04-06 23:06:51 -0500

    exploring more tdd

---
+++ src/test/kotlin/com/jolvera/jbud/services/ExpenseServiceTests.kt
@@ -0,0 +1,38 @@
+package com.jolvera.jbud.services
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class ExpenseServiceTests {
+    private lateinit var service: ExpenseService
+
+    @BeforeEach
+    fun setUp() {
+        service = ExpenseService()
+    }
+
+    @Test
+    fun fetchExpenseList() {
+
+    }
+
+    @Test
+    fun fetchExpenseListDifferentTypes() {
+
+    }
+
+    @Test
+    fun addExpenseToList() {
+
+    }
+
+    @Test
+    fun updateExpenseItemInList() {
+
+    }
+
+    @Test
+    fun removeExpenseItemInList() {
+
+    }
+}
---
+++ src/test/kotlin/com/jolvera/jbud/services/IncomeServiceTests.kt
@@ -0,0 +1,30 @@
+package com.jolvera.jbud.services
+
+import org.junit.jupiter.api.Test
+
+class IncomeServiceTests {
+    @Test
+    fun fetchIncomeList() {
+
+    }
+
+    @Test
+    fun fetchIncomeListDifferentTypes() {
+
+    }
+
+    @Test
+    fun addIncomeToList() {
+
+    }
+
+    @Test
+    fun updateIncomeItemInList() {
+
+    }
+
+    @Test
+    fun removeIncomeItemInList() {
+
+    }
+}
