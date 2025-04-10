148e1c8866afde8f195eb3194ebe9ada25216797
Jolvera2001 <jolvera6@stedwards.edu>
2025-04-09 22:23:43 -0500

    removed coroutine stuff in app startup

--- src/main/kotlin/com/jolvera/jbud/JBudApplication.kt
+++ src/main/kotlin/com/jolvera/jbud/JBudApplication.kt
@@ -7,15 +7,8 @@ import javafx.scene.control.ListView
 import javafx.scene.layout.BorderPane
 import javafx.scene.layout.StackPane
 import javafx.stage.Stage
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.javafx.JavaFx
 
 class JBudApplication : Application() {
-    val appScope = CoroutineScope(Dispatchers.JavaFx)
-
     override fun start(primaryStage: Stage?) {
         primaryStage?.isMaximized = true
 
@@ -54,10 +47,5 @@ class JBudApplication : Application() {
         primaryStage?.title = "JBud"
         primaryStage?.scene = scene
         primaryStage?.show()
-    }
-
-    override fun stop() {
-        appScope.cancel()
-        super.stop()
     }
 }
