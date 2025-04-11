8c072aeb865ea862b537f19db527f6af36540979
Jolvera2001 <jolvera6@stedwards.edu>
2025-04-11 16:55:07 -0500

    added KDocs for BaseView

--- src/main/kotlin/com/jolvera/jbud/lib/BaseView.kt
+++ src/main/kotlin/com/jolvera/jbud/lib/BaseView.kt
@@ -3,6 +3,19 @@ package com.jolvera.jbud.lib
 import org.koin.core.component.KoinComponent
 
 // meant for new views to implement (basically inherit)
+/**
+ * BaseView is meant for new views to implement and inherit.
+ * This interface inherits from KoinComponent to automatically
+ * provide the Koin DSL for using things like `by inject()`
+ *
+ * ## Functions
+ * - [onRemove] - **NEEDS** to be implemented to do any sort of cleanup.
+ * Cleanup should include the disposal of any used viewModel inheriting from
+ * `BaseViewModel`
+ *
+ * @see BaseViewModel
+ * @author Jolvera
+ */
 interface BaseView: KoinComponent {
     fun onRemove() // do viewmodel.dispose()
 }
